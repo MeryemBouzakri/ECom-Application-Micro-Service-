0117d77d62d387811b2636a69c95cfd1c9bd42ba
Meryem Bouzakri <bouzekrimeryem@gmail.com>
2024-11-11 10:30:34 +0100

    V1

---
+++ confg-service/src/main/java/org/meryem/confgservice/ConfgServiceApplication.java
@@ -0,0 +1,13 @@
+package org.meryem.confgservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfgServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfgServiceApplication.class, args);
+    }
+
+}
---
+++ confg-service/src/test/java/org/meryem/confgservice/ConfgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meryem.confgservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfgServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
