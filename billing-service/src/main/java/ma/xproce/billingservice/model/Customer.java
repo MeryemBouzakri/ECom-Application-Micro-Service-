dce4af0cfe6a503b5a1116180fe85ff27240cd88
Meryem Bouzakri <bouzekrimeryem@gmail.com>
2024-11-15 21:38:42 +0100

    Suite

--- billing-service/src/main/java/ma/xproce/billingservice/model/Customer.java
+++ billing-service/src/main/java/ma/xproce/billingservice/model/Customer.java
@@ -1,8 +1,12 @@
 package ma.xproce.billingservice.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class Customer {
     private Long id;
     private String Name;
--- billing-service/src/main/java/ma/xproce/billingservice/service/CustomerRestClient.java
+++ billing-service/src/main/java/ma/xproce/billingservice/service/CustomerRestClient.java
@@ -2,6 +2,7 @@ package ma.xproce.billingservice.service;
 
 import ma.xproce.billingservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.web.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CustomerRestClient {
     @GetMapping(path = "/customers/{id}")
     Customer findCustomerById(@PathVariable Long id);
+    @GetMapping("/api/customers")
+    PagedModel<Customer> getAllCustomers();
 
 
 }
